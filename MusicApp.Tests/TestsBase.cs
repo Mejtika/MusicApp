8254f588b1f1dc0b7c08294689ae2168e12cfe3e
Mateusz Golombniak <Mejtika@hotmail.com>
2021-06-13 23:57:03 +0200

    Add test project that targets xunit
    Add base class for integration tests - TestsBase, that is responsible for handling services initialization and database cleanup. It also exposes a set of handy helper methods for dispatching command/queries and populating database.

---
+++ MusicApp.Tests/TestsBase.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using MusicApp.IdentityData;
+using MusicApp.MusicData;
+using Respawn;
+using Xunit;
+
+namespace MusicApp.Tests
+{
+    public abstract class IntegrationTestBase : IAsyncLifetime
+    {
+        public virtual async Task InitializeAsync()
+        {
+            await TestsBase.ResetCheckpoint().ConfigureAwait(false);
+        }
+
+        public virtual Task DisposeAsync() => Task.CompletedTask;
+    }
+
+    public class TestsBase
+    {
+        private static readonly IServiceScopeFactory _scopeFactory;
+
+        private static readonly IConfiguration _configuration;
+
+        private static readonly Checkpoint _checkpoint;
+
+        static TestsBase()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile("appsettings.tests.json", false, true)
+                .AddEnvironmentVariables();
+
+            _configuration = builder.Build();
+            var startup = new Startup(_configuration);
+            var services = new ServiceCollection();
+
+            // Identity services need this.
+            services.AddSingleton(Mock.Of<IWebHostEnvironment>(w =>
+                w.EnvironmentName == "Development" &&
+                w.ApplicationName == "MusicApp"));
+            startup.ConfigureServices(services);
+            _checkpoint = new Checkpoint
+            {
+                TablesToIgnore = new[] { "__IdentityMigrationsHistory", "__IdentityMigrationsHistory" }
+            };
+            _scopeFactory = services.BuildServiceProvider().GetService<IServiceScopeFactory>();
+            EnsureDatabase();
+        }
+
+        public static void EnsureDatabase()
+        {
+            using var scope = _scopeFactory.CreateScope();
+
+            var identityContext = scope.ServiceProvider.GetService<IdentityDbContext>();
+            identityContext.Database.Migrate();
+
+            var musicContext = scope.ServiceProvider.GetService<MusicDataDbContext>();
+            musicContext.Database.Migrate();
+        }
+
+        public static Task ResetCheckpoint() => _checkpoint.Reset(_configuration.GetConnectionString("DefaultConnection"));
+
+        protected async Task AddApplicationUserAsync(ApplicationUser user)
+        {
+            using var scope = _scopeFactory.CreateScope();
+            var userManager = scope.ServiceProvider.GetService<UserManager<ApplicationUser>>();
+            await userManager.CreateAsync(user).ConfigureAwait(false);
+        }
+
+        public static async Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request)
+        {
+            using var scope = _scopeFactory.CreateScope();
+
+            var mediator = scope.ServiceProvider.GetService<IMediator>();
+
+            return await mediator.Send(request);
+        }
+
+        public static async Task AddAsync<TEntity>(TEntity entity)
+            where TEntity : class
+        {
+            using var scope = _scopeFactory.CreateScope();
+
+            var context = scope.ServiceProvider.GetService<MusicDataDbContext>();
+
+            await context.AddAsync(entity);
+
+            await context.SaveChangesAsync();
+        }
+
+        public static async Task AddRangeAsync<TEntity>(List<TEntity> entity)
+            where TEntity : class
+        {
+            using var scope = _scopeFactory.CreateScope();
+
+            var context = scope.ServiceProvider.GetService<MusicDataDbContext>();
+
+            await context.AddRangeAsync(entity);
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
