e6551a38f08463a52950ac083fa81be11f3f7fd5
Mateusz Golombniak <Mejtika@hotmail.com>
2021-06-21 11:32:27 +0200

    Add message service to user slice to display success messages

--- MusicApp/ClientApp/src/app/services/users/users.facade.ts
+++ MusicApp/ClientApp/src/app/services/users/users.facade.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { MessageService } from 'primeng/api';
 import { Subject } from 'rxjs';
 import { User, UserForCreate, UserForUpdate } from './interfaces';
 import { UsersService } from './users.service';
@@ -10,10 +11,11 @@ export class UsersFacade {
   allUsers$ = this.allUsers.asObservable();
   mutations$ = this.mutations.asObservable();
 
-  constructor(private usersService: UsersService) {}
+  constructor(private usersService: UsersService, private messageService: MessageService) {}
 
-  reset() {
+  reset(message: string) {
     this.mutations.next(true);
+    this.messageService.add({severity:'success', summary: 'Sukces', detail: message});
   }
 
   loadUsers() {
@@ -23,14 +25,14 @@ export class UsersFacade {
   }
 
   createUser(user: UserForCreate) {
-    this.usersService.create(user).subscribe((_) => this.reset());
+    this.usersService.create(user).subscribe((_) => this.reset("Pomyślnie utworzono nowego użytkownika."));
   }
 
   updateUser(user: UserForUpdate) {
-    this.usersService.update(user).subscribe((_) => this.reset());
+    this.usersService.update(user).subscribe((_) => this.reset("Pomyślnie zaktualizowano dane użytkownika."));
   }
 
   deleteUser(user: User) {
-    this.usersService.delete(user).subscribe((_) => this.reset());
+    this.usersService.delete(user).subscribe((_) => this.reset("Pomyślnie usunięto użytkownika."));
   }
 }
